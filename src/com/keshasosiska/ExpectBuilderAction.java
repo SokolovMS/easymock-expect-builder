d1727ef5fc829c00d6f70c2e10dddbe7eed4bf02
Mikhail Sokolov <m.s.sokolov.92@gmail.com>
2017-03-08 13:34:25 +0300

    Added generation of test file path

--- src/com/keshasosiska/ExpectBuilderAction.java
+++ src/com/keshasosiska/ExpectBuilderAction.java
@@ -1,62 +1,51 @@
 package com.keshasosiska;
 
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ScrollType;
-import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.openapi.editor.actionSystem.EditorAction;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
-import com.intellij.openapi.util.TextRange;
+import static com.google.common.base.Preconditions.checkArgument;
 
-public class ExpectBuilderAction extends EditorAction {
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
 
-    public ExpectBuilderAction(EditorActionHandler defaultHandler) {
-        super(defaultHandler);
+public class ExpectBuilderAction extends AnAction {
+    private static final String separator = "/";
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        VirtualFile sourceFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+        String fileName = sourceFile != null ? sourceFile.getName() : null;
+
+        VirtualFile testFile = getTestFile(sourceFile);
     }
 
-    public ExpectBuilderAction() {
-        this(new UpHandler());
+    private VirtualFile getTestFile(final VirtualFile sourceFile) {
+        VirtualFile testFile;
+
+        String sourcePath = sourceFile.getPath();
+        String testPath = makeTestPath(sourcePath);
+
+        return sourceFile;
     }
 
-    private static class UpHandler extends EditorWriteActionHandler {
-        private UpHandler() {
+    private String makeTestPath(final String sourcePath) {
+        String regex = separator + "main" + separator;
+        String[] parts = sourcePath.split(regex);
+        if (parts.length == 1) {
+            return makeTestFilename(sourcePath);
         }
 
-        @Override
-        public void executeWriteAction(Editor editor, DataContext dataContext) {
-            Document document = editor.getDocument();
-
-            if (editor == null || document == null || !document.isWritable()) {
-                return;
-            }
-
-            // CaretModel used to find caret position
-            CaretModel caretModel = editor.getCaretModel();
-            // SelectionModel used to find selection ranges
-            SelectionModel selectionModel = editor.getSelectionModel();
-
-            // get the range of the selected characters
-            TextRange charsRange = new TextRange(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd());
-            // get the range of the selected lines (block of code)
-            TextRange linesRange = new TextRange(document.getLineNumber(charsRange.getStartOffset()), document.getLineNumber(charsRange.getEndOffset()));
-            // range of the duplicated string
-            TextRange linesBlock = new TextRange(document.getLineStartOffset(linesRange.getStartOffset()), document.getLineEndOffset(linesRange.getEndOffset()));
-
-            // get the string to duplicate
-            String duplicatedString = document.getText().substring(linesBlock.getStartOffset(), linesBlock.getEndOffset());
-            duplicatedString += "\n";
-
-            // insert new duplicated string into the document
-            document.insertString(linesBlock.getStartOffset(), duplicatedString);
-
-            // select duplicated block
-            editor.getSelectionModel().setSelection(linesBlock.getStartOffset(), linesBlock.getStartOffset());
-            // move cursor to the start of copied block
-            caretModel.moveToOffset(linesBlock.getStartOffset());
-            editor.getScrollingModel().scrollToCaret(ScrollType.RELATIVE);
+        String testPath = parts[0];
+        for (int i = 1; i < parts.length - 1; i++) {
+            testPath += regex + parts[i];
         }
+        testPath += separator + "test" + separator + parts[parts.length - 1];
+
+        return makeTestFilename(testPath);
+    }
+
+    private String makeTestFilename(final String testPath) {
+        checkArgument(testPath.endsWith(".java"), "Can work only with java files");
+
+        return testPath.replace(".java", "TestBuilder.java");
     }
 }
