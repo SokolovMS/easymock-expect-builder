9cd70d452f014c8e9ba68e361fbd36bfd9b7aabe
Mikhail Sokolov <m.s.sokolov.92@gmail.com>
2017-04-06 23:13:43 +0300

    Refactored simple maven test project

---
+++ testprojects/maven-simple/src/main/java/com/keshasosiska/CalculatorImpl.java
@@ -0,0 +1,27 @@
+package com.keshasosiska;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class CalculatorImpl implements Calculator {
+    public String name;
+
+    public CalculatorImpl(final String name) {
+        this.name = name;
+    }
+
+    public int addInts(final int a, final int b) {
+        return a + b;
+    }
+
+    public String addStrings(final String a, final String b) {
+        return a + b;
+    }
+
+    public PublicClass addClasses(final PublicClass a, final PublicClass b) {
+        return new PublicClass();
+    }
+
+    public void voidMethod(final double a, final PublicClass b) {
+        throw new NotImplementedException();
+    }
+}
